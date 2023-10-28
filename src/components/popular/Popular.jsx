bd31194634894ea2f223334ec7245b405768b1b0
VaishakhPN <vaishakh.p@edstem.com>
2023-10-28 22:21:41 +0530

    final commit

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -4,23 +4,26 @@ import ThreeRobo from "../../assets/3men.png";
 import DroneGirl from "../../assets/drone-girl.png";
 import CyberPunk from "../../assets/cyberpunk.png";
 import FlyRobo from "../../assets/robot2.png";
+import "./Popular.css";
 
 const Popular =() =>{
     return(
-        <div>
-            <div>
-                <div>
-                    <div className="heading4">  
-                        <h1>Popular <span>Artists</span></h1>              
-                        <h1>On This Week</h1>
-                    </div>
-                    <button className="btbn">See All</button>
+        <div className="popular">
+            <div className="head-btn">
+                <div className="heading4">  
+                    <h1>Popular <span>Artists</span></h1>              
+                    <h1>On This Week</h1>
                 </div>
-                <div className="pics3">
+                <button className="btbn">See All</button>
+            </div>
+            <div className="pics3">
+                <div className="pics4">
                     <img className="creamrobo" alt="creamrobo" src={CreamRobo} />
                     <img className="3robot" alt="3robots" src={ThreeRobo} />
                     <img className="dronegirl" alt="dronegirl" src={DroneGirl} />
                     <img className="cyberpunk" alt="cyberpunk" src={CyberPunk} />
+                </div>
+                <div>
                     <img className="flyrobo" alt="flyrobo" src={FlyRobo} />
                 </div>
             </div>
